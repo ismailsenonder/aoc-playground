e4849a0ded254e64e38e922638511917d6aa8464
Ismail Senonder <i.senonder@gmail.com>
2021-12-05 02:37:09 +0100

    2021 day 2

---
+++ AdventOfCode/AOC2021/Day2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AOC2021
+{
+    public class Day2
+    {
+        private static string path = Environment.CurrentDirectory + @"\\Day2\input.txt";
+        public static int Part1_CalculateDepthAndHorizontalPosition()
+        {
+            string line = "";
+            string[] move;
+            int step = 0;
+            int horizontalValue = 0, verticalValue = 0;
+
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                move = line.Split(" ");
+                step = Convert.ToInt32(move[1]);
+                if (move[0] == "forward")
+                {
+                    horizontalValue = horizontalValue + step;
+                }
+                else if (move[0] == "down")
+                {
+                    verticalValue = verticalValue + step;
+                }
+                else if (move[0] == "up")
+                {
+                    verticalValue = verticalValue - step;
+                }
+                else
+                {
+                    //nothing
+                }
+            }
+            file.Close();
+
+            return horizontalValue * verticalValue;
+        }
+
+        public static int Part2_CalculateDepthAndHorizontalPositionWithAim()
+        {
+            string line = "";
+            string[] move;
+            int step = 0;
+            int horizontalValue = 0, verticalValue = 0, aim = 0;
+
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                move = line.Split(" ");
+                step = Convert.ToInt32(move[1]);
+                if (move[0] == "forward")
+                {
+                    horizontalValue = horizontalValue + step;
+                    verticalValue = verticalValue + (aim * step);
+                }
+                else if (move[0] == "down")
+                {
+                    aim = aim + step;
+                }
+                else if (move[0] == "up")
+                {
+                    aim = aim - step;
+                }
+                else
+                {
+                    //nothing
+                }
+            }
+            file.Close();
+
+            return horizontalValue * verticalValue;
+        }
+    }
+}
--- AdventOfCode/CodeRunner/Program.cs
+++ AdventOfCode/CodeRunner/Program.cs
@@ -7,8 +7,8 @@ namespace CodeRunner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Day1.Part1_MeasurementIncrementCount());
-            Console.WriteLine(Day1.Part2_ThreeMeasurementIncrementCount());
+            Console.WriteLine(Day2.Part1_CalculateDepthAndHorizontalPosition());
+            Console.WriteLine(Day2.Part2_CalculateDepthAndHorizontalPositionWithAim());
         }
     }
 }
