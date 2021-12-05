9d520dd48362c5f61234cb82cbf1df8bdb0e9ee3
Ismail Senonder <i.senonder@gmail.com>
2021-12-05 02:28:25 +0100

    aoc2021 day1

---
+++ AdventOfCode/AOC2021/Day1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021
+{
+    public class Day1
+    {
+        private static string path = Environment.CurrentDirectory + @"\\Day1\input.txt";
+        public static int Part1_MeasurementIncrementCount()
+        {
+            int prev = 0, curr = 0, count = 0;
+            string line;
+            
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                prev = curr;
+                curr = Convert.ToInt32(line);
+                if (curr > prev) count++;
+            }
+            file.Close();
+            return count - 1;
+        }
+
+        public static int Part2_ThreeMeasurementIncrementCount()
+        {
+            int index1 = 0, index2 = 0, index3 = 0,
+                count = 0, previousSum = 0, currentSum = 0, skipcount = 0;
+            string line;
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                index1 = index2;
+                index2 = index3;
+                index3 = Convert.ToInt32(line);
+
+                if (skipcount < 2)
+                {
+                    skipcount++;
+                    continue;
+                }
+
+                previousSum = currentSum;
+                currentSum = index1 + index2 + index3;
+                if (currentSum > previousSum) count++;
+            }
+            file.Close();
+            return count - 1;
+        }
+    }
+}
---
+++ AdventOfCode/CodeRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using AOC2021;
+using System;
+
+namespace CodeRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day1.Part1_MeasurementIncrementCount());
+            Console.WriteLine(Day1.Part2_ThreeMeasurementIncrementCount());
+        }
+    }
+}
