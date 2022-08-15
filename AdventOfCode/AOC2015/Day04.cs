4cda7ff8c75c5289ccda68fe5de250ed096d66c6
Ismail Senonder <i.senonder@gmail.com>
2022-08-16 01:40:48 +0200

    2015 day 4

---
+++ AdventOfCode/AOC2015/Day04.cs
@@ -0,0 +1,51 @@
+﻿namespace AOC2015
+{
+    public class Day04
+    {
+        private static readonly string path = Environment.CurrentDirectory + @"\\Inputs\Day04.txt";
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes); // .NET 5 +
+
+                // Convert the byte array to hexadecimal string prior to .NET 5
+                // StringBuilder sb = new System.Text.StringBuilder();
+                // for (int i = 0; i < hashBytes.Length; i++)
+                // {
+                //     sb.Append(hashBytes[i].ToString("X2"));
+                // }
+                // return sb.ToString();
+            }
+        }
+
+        public static string Part1(string privateKey, int zeroesToCheck)
+        {
+            string zeroesString = "";
+            for (int i = 0; i < zeroesToCheck; i++)
+            {
+                zeroesString += "0";
+            }
+
+            int numberToAdd = 1;
+            string strToCheck, adventCoin;
+            while (true)
+            {
+                strToCheck = privateKey + numberToAdd.ToString();
+                adventCoin = CreateMD5(strToCheck);
+                if (adventCoin[..zeroesToCheck] == zeroesString)
+                {
+                    return strToCheck;
+                }
+                else
+                {
+                    numberToAdd++;
+                }
+            }
+        }
+    }
+}
--- AdventOfCode/AOC2015/Program.cs
+++ AdventOfCode/AOC2015/Program.cs
@@ -1,10 +1,19 @@
 ﻿using AOC2015;
 
+//Day-01
 //Console.WriteLine(Day01.Part1());
 //Console.WriteLine(Day01.Part2());
 
+//Day-02
 //var values = Day02.Part1And2();
 //Console.WriteLine(values.total + " " + values.ribbon);
 
-Console.WriteLine(Day03.Part1());
-Console.WriteLine(Day03.Part2());
+//Day-03
+//Console.WriteLine(Day03.Part1());
+//Console.WriteLine(Day03.Part2());
+
+//Day-04
+Console.WriteLine(Day04.Part1("abcdef", 5));
+Console.WriteLine(Day04.Part1("pqrstuv", 5));
+Console.WriteLine(Day04.Part1("bgvyzdsv", 5));
+Console.WriteLine(Day04.Part1("bgvyzdsv", 6));
